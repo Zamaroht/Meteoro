5c2bc33f5502c9251cb629b86551a639bf6f330a
Zamaroht <laurito200@gmail.com>
2015-09-01 19:06:00 -0300

    Placeholder art. Movimientos temporales para la nave. Camara haciendo follow, podria suavizarse el movimiento

--- Meteor Revenge/Assets/CameraController.cs
+++ Meteor Revenge/Assets/CameraController.cs
@@ -5,15 +5,44 @@ public class CameraController : MonoBehaviour
 {
 	// Se encarga de seguir al jugador en Y
 
+	public float followThreshold;	// Distancia a la que tiene que estar el jugador del borde de la pantalla para que se mueva
+
 	private GameObject player;
+
+	private float last_player_y;
 
 	void Start () 
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
+
+		last_player_y = player.transform.position.y;
 	}
 
 	void Update () 
 	{
-	
+		Camera cam = GetComponent<Camera> ();
+		float top = cam.ScreenToWorldPoint(new Vector2(0, cam.pixelHeight)).y;
+		float bottom = cam.ScreenToWorldPoint(new Vector2(0, 0)).y;
+
+		if (player.transform.position.y > top - followThreshold) 
+		{
+			float deltay = player.transform.position.y - last_player_y;
+
+			transform.position = new Vector3 (
+				transform.position.x,
+				transform.position.y + deltay,
+				transform.position.z);
+		} 
+		else if (player.transform.position.y < bottom + followThreshold)
+		{
+			float deltay = player.transform.position.y - last_player_y;
+			
+			transform.position = new Vector3 (
+				transform.position.x,
+				transform.position.y + deltay,
+				transform.position.z);
+		} 
+
+		last_player_y = player.transform.position.y;
 	}
 }
--- Meteor Revenge/Assets/player/PlayerController.cs
+++ Meteor Revenge/Assets/player/PlayerController.cs
@@ -5,13 +5,42 @@ public class PlayerController : MonoBehaviour
 {
 	// Se encarga de los controles de la nave
 
+	private float speed;
+
 	void Start () 
 	{
-	
+		speed = 0.15f;
 	}
 
 	void Update () 
 	{
-	
+		
 	}
+
+	void FixedUpdate()
+	{
+		// Controles temporales, despues los hacemos mas lindos
+
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) 	// Aceleracion
+		{
+			transform.position = transform.position + (transform.up * speed);
+		}
+
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 	// Rotacion izquierda
+		{
+			transform.Rotate(new Vector3(0, 0, 4f));
+		}
+
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 	// Rotacion derecha
+		{
+			transform.Rotate(new Vector3(0, 0, -4f));
+		}
+
+		if (Input.GetKey (KeyCode.J)) 	// Magnet
+		{
+			
+		}
+
+	}
+
 }
