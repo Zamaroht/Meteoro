bdea90f88d570ce41050d653696e066c73ad266f
Zamaroht <laurito200@gmail.com>
2015-09-01 17:55:44 -0300

    Modificada un poco la estructura de componentes; agregado prefb esqueleto del meteorito chico

---
+++ Meteor Revenge/Assets/meteor/MeteorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorController : MonoBehaviour 
+{
+	// Controla el movimiento y las colisiones del meteoro
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
