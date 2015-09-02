1f7c16caf2bec46cb5900f12b75f4aca67edce32
Nochi Gao <gao.carlos95@gmail.com>
2015-09-02 00:08:54 -0300

    añadido un GameController.

---
+++ Meteor Revenge/Assets/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	//Controla algunos aspectos basicos
+
+	public GameObject player;
+	public bool gameStarted = false;
+
+
+	void Awake () {
+	
+		SpawnPlayer ();
+
+	}
+
+	void SpawnPlayer()
+	{
+		GameObject newPlayer = (GameObject)Instantiate (player, Vector3.zero, Quaternion.identity);
+		gameStarted = true;
+	}
+}
