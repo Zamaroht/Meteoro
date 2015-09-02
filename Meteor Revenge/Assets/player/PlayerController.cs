7e319bc749b0726fbb47fb06f04b4e457e64332e
Nochi Gao <gao.carlos95@gmail.com>
2015-09-01 23:45:23 -0300

    añadida aceleración, desaceleración, y velocidad mínima constante

--- Meteor Revenge/Assets/player/PlayerController.cs
+++ Meteor Revenge/Assets/player/PlayerController.cs
@@ -5,11 +5,14 @@ public class PlayerController : MonoBehaviour
 {
 	// Se encarga de los controles de la nave
 
-	private float speed;
+	private float speed, minSpeed, maxSpeed, acceleration;
 
 	void Start () 
 	{
-		speed = 0.15f;
+		speed = 0.0f;
+		minSpeed = 0.015f;
+		maxSpeed = 0.15f;
+		acceleration = 0.15f;
 	}
 
 	void Update () 
@@ -19,28 +22,51 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		MovementInput ();
+		MagnetInput ();
+	}
+
+
+	void MovementInput()
+	{
 		// Controles temporales, despues los hacemos mas lindos
 
-		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) 	// Aceleracion
+		transform.position = transform.position + (transform.up * speed); // Movimiento Constante
+
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow))	// Aceleracion
 		{
-			transform.position = transform.position + (transform.up * speed);
+			speed = speed + (acceleration * Time.deltaTime);
+		}
+		else
+		{ 
+			speed = speed - (acceleration * 2 *Time.deltaTime);	// Desaceleracion
 		}
 
 		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 	// Rotacion izquierda
 		{
 			transform.Rotate(new Vector3(0, 0, 4f));
 		}
-
+		
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 	// Rotacion derecha
 		{
 			transform.Rotate(new Vector3(0, 0, -4f));
 		}
 
-		if (Input.GetKey (KeyCode.J)) 	// Magnet
+		if (speed <= minSpeed)	//Velocidad Maxima
+		{ 
+			speed = minSpeed;
+		}
+		if (speed >= maxSpeed)	//Velocidad Minima
+		{ 
+			speed = maxSpeed;
+		}
+	}
+
+	void MagnetInput()
+	{
+		if (Input.GetKey (KeyCode.J))	// Magnet
 		{
 			
 		}
-
 	}
-
 }
